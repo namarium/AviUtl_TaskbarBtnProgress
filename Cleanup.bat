8e3a2749bbf9afa794c33b1a383590b2303e3d8d
namarium <namarium.dev@gmail.com>
2025-08-30 23:44:18 +0900

    Refactor Cleanup.bat to support multiple VS versions
    Updated Cleanup.bat to loop over a list of Visual Studio versions (vs2019 and vs2022) for cleaning build directories. This makes the cleanup process more flexible and easier to maintain as new versions are added.

--- Cleanup.bat
+++ Cleanup.bat
@@ -1,15 +1,19 @@
 @pushd %~dp0
 
+@set "list=vs2019 vs2022"
+@for %%v in (%list%) do (
+
 @rem Solution
-rd /s /q .\Solution\vs2019\.vs
-rd /s /q .\Solution\vs2019\Debug
-rd /s /q .\Solution\vs2019\Release
+rd /s /q .\Solution\%%v\.vs
+rd /s /q .\Solution\%%v\Debug
+rd /s /q .\Solution\%%v\Release
 
 @rem TaskbarBtnProgress
-rd /s /q .\TaskbarBtnProgress\Project\vs2019\Debug
-rd /s /q .\TaskbarBtnProgress\Projects\vs2019\Release
+rd /s /q .\TaskbarBtnProgress\Project\%%v\Debug
+rd /s /q .\TaskbarBtnProgress\Projects\%%v\Release
 
 @rem __ExtraLibs__
 
+)
 
 @popd
